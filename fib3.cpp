2de9e4e588dcabae13dc389e908e2ca77afabae0
darienli <166656631+darienli@users.noreply.github.com>
2024-04-10 23:30:08 +0300

    Changed to iterative way to create the Fibonacci sequence that starts from 0, 1, ..., n

---
+++ fib3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+void fib(int n){ 
+    int previous_el = 1; int current_el = 1;
+    if (n > 1) {
+        cout << "0"<<"\n"<< "1"<<"\n";
+        for (int k = 2; k < n; ++k){
+            cout << current_el << "\n";
+            current_el +=previous_el;
+            previous_el = current_el - previous_el;
+        }
+    }
+    else cout<<"0";
+}
+
+int main(){
+    int n; 
+    cout << "n: "; cin >> n;
+    fib(n);
+}
+
